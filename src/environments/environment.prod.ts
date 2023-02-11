cb5384570d77e6472c5af656f2ac402cb645c150
Sankalp Raibole <Sankalp.Raibole@lntinfotech.com>
2023-02-11 22:52:40 +0530

    chnage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 
 export const environment = {
-    isProduction : false,
+    isProduction : true,
     firebase : {
-        apiKey: `${process.env['apiKey']}`,
-        authDomain: `${process.env['authDomain']}`,
-        projectId: `${process.env['projectId']}`,
-        storageBucket: `${process.env['storageBucket']}`,
-        messagingSenderId:`${process.env['messagingSenderId']}`,
-        appId: `${process.env['appId']}`,
+        apiKey: "AIzaSyDNLf0uln9b5pS7pfAgd0C65iWtzWqxLf8",
+        authDomain: "clips-d1242.firebaseapp.com",
+        projectId: "clips-d1242",
+        storageBucket: "clips-d1242.appspot.com",
+        messagingSenderId: "670288000716",
+        appId: "1:670288000716:web:7d8de2d2048d198c353469"
     }
 }
